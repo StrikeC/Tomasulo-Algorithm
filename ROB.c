65205f81ce2cc1e38366a717ac62c43b61f54a8a
Ian Chen <tianqichen@Tianqis-MacBook-Air.local>
2016-12-14 16:52:00 -0600

    fix commit

--- ROB.c
+++ ROB.c
@@ -697,53 +697,57 @@ void checkBroadcast()
  */
 bool checkCommit()
 {
-    if( rob[commitPointer].exception ) // destroy everything and exit
-    {
-        // clear rob
-        for( uint8_t i = 1; i <= 6; i++ )
-        {
-            rob[i].busy = rob[i].done = rob[i].exception = false;
-            rob[i].op = rob[i].dst = rob[i].value = 0;
-        }
-        
-        // clear rs
-        for( uint8_t i = 1; i <= 5; i++ )
-        {
-            rs[i].busy = false;
-            rs[i].op = rs[i].dst = rs[i].vj = rs[i].vk = rs[i].qj = rs[i].qk = 0;
-        }
-        
-        // clear rat
-        memset(registerAllocationTable, 0, sizeof(registerAllocationTable));
-        
-        // return exit
-        return true;
-        
-    }
-    else
-    {
-        // reset rob
-		temp.busyCom = true;
-        temp.robDstCom = commitPointer;
-        
-        if( registerAllocationTable[rob[commitPointer].dst] = commitPointer )
-        {
-            registerAllocationTable[rob[commitPointer].dst] = 0;
-            registerFile[rob[commitPointer].dst] = rob[commitPointer].value;
-        }
-
-        // update commit pointer
-        if( commitPointer == 6 )
-        {
-            commitPointer = 1;
-        }
-        else
-        {
-            commitPointer++;
-        }
-
-        return false;
-    }
+    if( rob[commitPointer].done )
+	{
+		if( rob[commitPointer].exception ) // destroy everything and exit
+		{
+			// clear rob
+			for( uint8_t i = 1; i <= 6; i++ )
+			{
+				rob[i].busy = rob[i].done = rob[i].exception = false;
+				rob[i].op = rob[i].dst = rob[i].value = 0;
+			}
+			
+			// clear rs
+			for( uint8_t i = 1; i <= 5; i++ )
+			{
+				rs[i].busy = false;
+				rs[i].op = rs[i].dst = rs[i].vj = rs[i].vk = rs[i].qj = rs[i].qk = 0;
+			}
+			
+			// clear rat
+			memset(registerAllocationTable, 0, sizeof(registerAllocationTable));
+			
+			// return exit
+			return true;
+			
+		}
+		else
+		{
+			// reset rob
+			temp.busyCom = true;
+			temp.robDstCom = commitPointer;
+			
+			// update RAT/RF
+			if( registerAllocationTable[rob[commitPointer].dst] = commitPointer )
+			{
+				registerAllocationTable[rob[commitPointer].dst] = 0;
+				registerFile[rob[commitPointer].dst] = rob[commitPointer].value;
+			}
+			
+			// update commit pointer
+			if( commitPointer == 6 )
+			{
+				commitPointer = 1;
+			}
+			else
+			{
+				commitPointer++;
+			}
+			
+			return false;
+		}
+	}
 }
 
 /*
